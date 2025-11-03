006fd41b6c8c0dce58d125dfee2971f5b1ffe275
meronemo <seanchoi227@gmail.com>
2025-11-03 12:57:20 +0900

    feat: apply sentry

---
+++ instrumentation-client.ts
@@ -0,0 +1,60 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://27d3369f0db62637c43b0e7adc94f454@o1306592.ingest.us.sentry.io/4510298135330816",
+
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "system",
+      isEmailRequired: true,
+      showBranding: false,
+      triggerLabel: "오류 제보",
+      triggerAreaLabel: "오류 제보",
+      formTitle: "오류 제보",
+      submitButtonLabel: "제출",
+      cancelButtonLabel: "취소",
+      addScreenshotButtonLabel: "스크린샷 추가",
+      removeScreenshotButtonLabel: "스크린샷 제거",
+      nameLabel: "이름",
+      namePlaceholder: "이름",
+      emailLabel: "이메일",
+      emailPlaceholder: "example@hafs.hs.kr",
+      isRequiredLabel: "(필수)",
+      messageLabel: "설명",
+      messagePlaceholder: "어떤 오류인가요? 어떤 상황에서 발생한 것인지 구체적으로 적어주세요.",
+      successMessageText: "제보해주셔서 감사합니다!",
+      highlightToolText: "강조하기",
+      hideToolText: "가리기",
+    }),
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
---
+++ sentry.server.config.ts
@@ -0,0 +1,26 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+import { nodeProfilingIntegration } from "@sentry/profiling-node"
+
+Sentry.init({
+  dsn: "https://27d3369f0db62637c43b0e7adc94f454@o1306592.ingest.us.sentry.io/4510298135330816",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing must be enabled for profiling to work
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
+  profileLifecycle: 'trace',
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
